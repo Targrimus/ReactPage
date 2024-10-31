c2d24dead526d5e1c0d3b88822ae40306b5efeed
RTK <tolga.kiran@outlook.com>
2024-10-31 03:03:40 +0300

    yarisi

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 import { Container} from 'react-bootstrap';
-import OffcanvasExample from './components/OffcanvasExample';
+import Offcanvas from './components/Offcanvas';
+import { useEffect, useState } from 'react';
+import LoadingSpinner from './components/LoadingSpinner'
+import Footer from './components/Footer'
 
 
 function App() {
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState();
+
+  useEffect(()=>{
+
+    fetch('./data.new.json')
+    .then(result=>result.json())
+    .then((data)=>{
+      setIsLoading(false);
+      setData(data)
+      console.log(data);
+    })
+    .catch(e=>console.log(e));
+
+  },[])
+
+
   return (
-      <Container fluid>
-        <OffcanvasExample/>
+      <Container fluid className='p-0 App'> 
+
+        <Offcanvas/>
+
+        {isLoading && <LoadingSpinner/>}
+
+        <Footer/>
+
+      
       </Container>
   );
 }
